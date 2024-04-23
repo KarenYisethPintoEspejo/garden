8f731beb126fcbd8c611e27616671ac0828c805e
CampusLands <139265002+CampusLands@users.noreply.github.com>
2024-04-23 12:21:23 -0500

    feat: :card_file_box: Se creo los json a consumir con sus modulos
    Consultas 1,2,3,4 y 5 realizadas

---
+++ js/main.js
@@ -0,0 +1,12 @@
+import { 
+    getAllOfficesCodeAndCity, 
+    getAllOfficesFromSpainCityAndMovil 
+} from "./module/offices.js";
+
+import { 
+    getAllEmployeesWithBossAndCodeSeven,
+    getBossFullNameAndEmail,
+    getAll
+} from "./module/employees.js";
+
+console.log(await getAll());
