73e9092962c09cb5874ff463495b903cadebe7f5
KarenYisethPintoEspejo <156373335+KarenYisethPintoEspejo@users.noreply.github.com>
2024-04-29 15:25:24 -0500

    feat: :sparkles: Ejercicio 1 ensamblado

--- js/main.js
+++ js/main.js
@@ -1,64 +1,14 @@
-// import { 
-//     getAllOfficesCodeAndCity, 
-//     getAllOfficesFromSpainCityAndMovil 
-// } from "./module/offices.js"
-
-// import { 
-//     getAllEmployeesWithBossAndCodeSeven,
-//     getBossFullNameAndEmail,
-//     getAll
-// } from "./module/employees.js";
-
-// import { 
-//     getAllClients
-// } from "./module/clients.js";
-
-// console.log(await getAllClients());
-
-// import {
-//     getAllStatus
-// } from "./module/requests.js";
-
-// console.log(await getAllStatus());
+import {getAllOfficesCodeAndCity} from "./module/offices.js"
 
 
-// import {
-//     getALLCodeRequests2008
-// } from "./module/requests.js";
-
-// console.log(await getAllCodeClients2008());
-
-// import { getAllCodeRequestLate } from "./module/requests.js";
-// console.log(await getAllCodeRequestLate()) 
 
 
-// import { getAllCodeTwoDays } from "./module/requests.js";
-// console.log(await getAllCodeTwoDays());
-
-// import {getAllRejected2009} from "./module/requests.js";
-// console.log(await getAllRejected2009())
-
-// import { getAllDeliveredJanuary } from "./module/requests.js";
-// console.log(await getAllDeliveredJanuary())
-
-// import { getAllPaymentsPayPal2008 } from "./module/payments.js";
-// console.log(await getAllPaymentsPayPal2008());
-
-// import {getAllPaymentMethods} from "./module/payments.js"
-// console.log(await getAllPaymentMethods());
 
 
-// import { getAllGamaOrnamentales } from "./module/products.js";
-// console.log(await getAllGamaOrnamentales());
 
 
-// import { getAllClientsMadrid1130 } from "./module/clients.js";
-// console.log(await getAllClientsMadrid1130());
 
 
-// import {getClientAndSaleAgentFullName} from "./module/clients.js"
-// console.log(await getClientAndSaleAgentFullName());
+//PRIMERA PARTE - 16 EJERCICIOS
+console.log(await getAllOfficesCodeAndCity()); //1 
 
-
-import {getClientAndSaleAgentFullName2} from "./module/clients.js"
-console.log(await getClientAndSaleAgentFullName2());
--- js/report.js
+++ js/report.js
@@ -1,10 +1,24 @@
-import "./components/clock.js";
-import { getClientsEmploy } from "./module/clients.js";
-const queryAboutTable7 = document.querySelector("#queryAboutTable7");
-queryAboutTable7.addEventListener("click", async(e)=>{
-    let [,report__container] = queryAboutTable7.children
+// import "./components/clock.js";  
+import { getAllOfficesCodeAndCity } from "./module/offices.js";
+
+
+
+
+
+
+const queryAboutTable1= document.querySelector("#queryAboutTable1");
+
+
+
+
+
+
+
+
+queryAboutTable1.addEventListener("click", async(e)=>{
+    let [,report__container] = queryAboutTable1.children
     if(!report__container.innerHTML){
-        let data = await getClientsEmploy();
+        let data = await getAllOfficesCodeAndCity();
         let plantilla = "";
         console.log(data);
         data.forEach(val => {
@@ -15,8 +29,37 @@ queryAboutTable7.addEventListener("click", async(e)=>{
                 </div>
                 <div class="card__body">
                     <div class="body__marck">
-                        <p><b>Nombre del empleado: </b>${val.name_employee}</p>
-                        <p><b>Ciudad: </b>${val.city}</p>
+                        <p><b>Codigo_de_oficina </b>${val.codigo}</p>
+                        <p><b>Ciudad: </b>${val.ciudad}</p>
+                    </div>
+                </div>
+            </div>
+            `;
+        });
+        report__container.innerHTML = plantilla;
+    }
+})
+
+
+// 2. Devuelve un listado con la ciudad y el teléfono de las oficinas de España.
+
+
+queryAboutTable2.addEventListener("click", async(e)=>{
+    let [,report__container] = queryAboutTable2.children
+    if(!report__container.innerHTML){
+        let data = await getAllOfficesCodeAndCity();
+        let plantilla = "";
+        console.log(data);
+        data.forEach(val => {
+            plantilla += `
+                <div class="report__card">
+                <div class="card__title">
+                    <div>${val.client_name}</div>
+                </div>
+                <div class="card__body">
+                    <div class="body__marck">
+                        <p><b>Codigo_de_oficina </b>${val.codigo}</p>
+                        <p><b>Ciudad: </b>${val.ciudad}</p>
                     </div>
                 </div>
             </div>
